b289326e5d8ff6b360ee7dc4ce6078fb122c95c6
Juanma Gómez <jgomez@centeco.es>
2023-07-27 09:37:42 +0200

    06 - Cambio de uso del Hook useState por useReducer para gestionar estados más complejos en nuestra aplicación. Aparte se ha utilizado el paquete use-reducer-logger para tener logs de nuestro estado.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,44 @@
 import { Link } from 'react-router-dom';
 //import data from '../data';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import axios from 'axios';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
 
 function HomeScreen() {
+  //UTILIZAMOS REDUCER EN VEZ DE useState() PARA GESTIONAR EL ESTADO
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  });
+
   //OBTENER PRODUCTOS MEDIANTE PETICIÓN AXIOS AL BACKEND
-  const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({ type: 'FETCH_REQUEST' });
+
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
     };
     fetchData();
   }, []);
@@ -18,22 +47,28 @@ function HomeScreen() {
     <div>
       <h1>Productos destacados</h1>
       <div className="products">
-        {products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
+        {loading ? (
+          <div>Cargando...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
+            <div className="product" key={product.slug}>
               <Link to={`product/${product.slug}`}>
-                <p>{product.name}</p>
+                <img src={product.image} alt={product.name} />
               </Link>
-              <p>
-                <strong>{product.price}€</strong>
-              </p>
-              <button>Añadir a la cesta</button>
+              <div className="product-info">
+                <Link to={`product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>{product.price}€</strong>
+                </p>
+                <button>Añadir a la cesta</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
